2d10badcc7bed548c662424dcc368c043a3ccdb9
jb1361 <justinbutler4@hotmail.com>
2017-02-21 14:19:07 -0500

    next

---
+++ thingfornick/Assets/model/force.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class force : MonoBehaviour {
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = this.GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward * -5000);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+}
